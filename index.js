20c68b6b67037be5a33627195bb8a8cdd320d55a
David Reichardt <davereichardt@gmail.com>
2025-03-11 14:40:28 -0400

    added an if to check if scores are tied

--- index.js
+++ index.js
@@ -55,9 +55,17 @@ function playGame(numberOfGames) {
     console.log(`Computer: ${computerScore}\nYou: ${humanScore}`);
   }
 
-  if (computerScore > humanScore) {
-    console.log(`Final results:\nComputer:${computerScore}\nYou:${humanScore}\nComputer wins the game! Better luck next time.`);
+  if (computerScore === humanScore) {
+    console.log(
+      `Final results:\nComputer:${computerScore}\nYou:${humanScore}\nIt's a draw! Nobody wins.`
+    );
+  } else if (computerScore > humanScore) {
+    console.log(
+      `Final results:\nComputer:${computerScore}\nYou:${humanScore}\nComputer wins the game! Better luck next time.`
+    );
   } else {
-    console.log(`Final result:\nComputer:${computerScore}\nYou:${humanScore}\nYou win the game! Good job.`);
+    console.log(
+      `Final result:\nComputer:${computerScore}\nYou:${humanScore}\nYou win the game! Good job.`
+    );
   }
 }
