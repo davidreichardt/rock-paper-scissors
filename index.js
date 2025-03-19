5ecc356a4596155b6a52f054f4865980c82217f2
David Reichardt <davereichardt@gmail.com>
2025-03-18 23:59:48 -0400

    refactoring javascript to update DOM

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissors = document.getElementById('scissors');
 let humanScore = 0;
 let computerScore = 0;
+
+rock.addEventListener('click', clicked);
+paper.addEventListener('click', clicked);
+scissors.addEventListener('click', clicked);
+
+function clicked(humanChoice) {
+  humanChoice = this.id;
+  playRound(humanChoice);
+}
 
 function getComputerChoice() {
   let choice;
@@ -16,13 +28,27 @@ function getComputerChoice() {
   return choice;
 }
 
-function getHumanChoice() {
-  let userInput = window.prompt("Enter 'Rock', 'Paper', or 'Scissors'... ");
-  return userInput.toLowerCase();
+function updateScore() {
+  humanScore = 0;
+  computerScore = 0;
 }
 
-function playRound(computerChoice, humanChoice) {
+function appendMatchResult(matchResultMessage) {
+  let roundDescription = document.querySelector('.round-description');
+  matchResultMessage.classList.add('message');
+  roundDescription.appendChild(matchResultMessage);
+}
+
+function playRound(humanChoice) {
+  console.log(humanChoice);
+  let computerChoice = getComputerChoice();
+
+  let matchResultMessage = document.createElement('p');
+  console.log(computerChoice);
+
   if (computerChoice === humanChoice) {
+    matchResultMessage.textContent = `Computer chose ${computerChoice} and you chose ${humanChoice}, it's a draw!`;
+    appendMatchResult(matchResultMessage);
     console.log(
       `Computer chose ${computerChoice} and you chose ${humanChoice}, it's a draw!`
     );
@@ -39,33 +65,6 @@ function playRound(computerChoice, humanChoice) {
     humanScore++;
     console.log(
       `Computer chose ${computerChoice} and you chose ${humanChoice}, you win!`
-    );
-  }
-  return computerScore, humanScore;
-}
-
-function playGame(numberOfGames) {
-  let iteration = 0;
-
-  while (iteration < numberOfGames) {
-    iteration++;
-    const computerSelection = getComputerChoice();
-    const humanSelection = getHumanChoice();
-    playRound(computerSelection, humanSelection);
-    console.log(`Computer: ${computerScore}\nYou: ${humanScore}`);
-  }
-
-  if (computerScore === humanScore) {
-    console.log(
-      `Final results:\nComputer:${computerScore}\nYou:${humanScore}\nIt's a draw! Nobody wins.`
-    );
-  } else if (computerScore > humanScore) {
-    console.log(
-      `Final results:\nComputer:${computerScore}\nYou:${humanScore}\nComputer wins the game! Better luck next time.`
-    );
-  } else {
-    console.log(
-      `Final result:\nComputer:${computerScore}\nYou:${humanScore}\nYou win the game! Good job.`
     );
   }
 }
