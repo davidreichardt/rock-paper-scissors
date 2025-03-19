d2536b72c26d58f31fbf685065bc510af1025d79
David Reichardt <davereichardt@gmail.com>
2025-03-19 16:56:17 -0400

    javascript functions to run game with a ui

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const rock = document.getElementById('rock');
 const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
+const roundDescription = document.querySelector('.round-description');
 let humanScore = 0;
 let computerScore = 0;
+const humanScoreboard = document.getElementById('human-score');
+const computerScoreboard = document.getElementById('computer-score');
 
 rock.addEventListener('click', clicked);
 paper.addEventListener('click', clicked);
@@ -28,27 +31,60 @@ function getComputerChoice() {
   return choice;
 }
 
-function updateScore() {
+function resetGame() {
+  let message = document.querySelector('.message');
+  let message2 = document.querySelector('.message2');
   humanScore = 0;
   computerScore = 0;
+  humanScoreboard.innerText = humanScore;
+  computerScoreboard.innerText = computerScore;
+  message.remove();
+  message2.remove();
 }
 
-function appendMatchResult(matchResultMessage) {
-  let roundDescription = document.querySelector('.round-description');
+function updateScore() {
+  humanScoreboard.innerText = humanScore;
+  computerScoreboard.innerText = computerScore;
+  const winnerMessage = document.createElement('p');
+  winnerMessage.classList.add('message2');
+
+  if (computerScore === 5) {
+    winnerMessage.textContent = 'Computer won this game!';
+    roundDescription.appendChild(winnerMessage);
+    setTimeout(resetGame, 1000);
+  }
+  else if (humanScore === 5) {
+    winnerMessage.textContent = 'You won this game!';
+    roundDescription.appendChild(winnerMessage);
+    setTimeout(resetGame, 1000);
+  }
+}
+
+function addMessage(matchResultMessage) {
   matchResultMessage.classList.add('message');
   roundDescription.appendChild(matchResultMessage);
 }
 
-function playRound(humanChoice) {
-  console.log(humanChoice);
-  let computerChoice = getComputerChoice();
+function appendMatchResult(matchResultMessage) {
+  let existingMessage = document.querySelector('.message');
+  let messageExists = existingMessage ? true : false;
 
+  if (!messageExists) {
+    addMessage(matchResultMessage);
+  } else {
+    existingMessage.remove();
+    addMessage(matchResultMessage);
+  }
+}
+
+function playRound(humanChoice) {
+  let computerChoice = getComputerChoice();
   let matchResultMessage = document.createElement('p');
-  console.log(computerChoice);
 
   if (computerChoice === humanChoice) {
     matchResultMessage.textContent = `Computer chose ${computerChoice} and you chose ${humanChoice}, it's a draw!`;
     appendMatchResult(matchResultMessage);
+    updateScore();
     console.log(
       `Computer chose ${computerChoice} and you chose ${humanChoice}, it's a draw!`
     );
@@ -57,14 +93,20 @@ function playRound(humanChoice) {
     (computerChoice === 'scissors' && humanChoice === 'paper') ||
     (computerChoice === 'paper' && humanChoice === 'rock')
   ) {
+    matchResultMessage.textContent = `Computer chose ${computerChoice} and you chose ${humanChoice}, you lose this round!`;
+    appendMatchResult(matchResultMessage);
     computerScore++;
+    updateScore();
     console.log(
-      `Computer chose ${computerChoice} and you chose ${humanChoice}, you lose!`
+      `Computer chose ${computerChoice} and you chose ${humanChoice}, you lose this round!`, humanScore, computerScore
     );
   } else {
+    matchResultMessage.textContent = `Computer chose ${computerChoice} and you chose ${humanChoice}, you win this round!`;
+    appendMatchResult(matchResultMessage);
     humanScore++;
+    updateScore();
     console.log(
-      `Computer chose ${computerChoice} and you chose ${humanChoice}, you win!`
+      `Computer chose ${computerChoice} and you chose ${humanChoice}, you win this round!`, computerScore, humanScore
     );
   }
 }
